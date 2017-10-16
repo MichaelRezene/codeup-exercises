a5b1c9a3807b272056e4700429b5484d57ccc2e5
michael <mgebregziabher86@gmail.com>
2017-10-16 11:55:03 -0500

    completed the StringUtility exercise

--- src/main/java/com/codeup/App.java
+++ src/main/java/com/codeup/App.java
@@ -1,12 +1,27 @@
 package com.codeup;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter something: ");
+        String line = input.nextLine();
+
+        if (StringUtils.isNumeric(line)){
+            System.out.println(line + "Is a number");
+        }else {
+            System.out.println(line + " is not a number");
+        }
+        System.out.println("Flipped case: " + StringUtils.swapCase(line));
+        System.out.println("Reversed: " + StringUtils.reverse(line));
+
     }
 }
